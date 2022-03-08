093a97d61e9445b0f4c4f73a98719092e2519872
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-03-08 15:11:06 +0100

    Changement des noms de fonctions et variables en anglais

--- projet2image.py
+++ projet2image.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -40,4 +41,48 @@ with open(book_title+'_pic.jpg', 'wb') as handle:
             if not block:
                 break
 
+=======
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+
+
+#si on veut demander l'url du livre dans le terminal:
+print('Copy/Paste Book url, please:')
+url = input ()
+
+def get_img_url(url):
+		response_img = requests.get(url)
+		page_img = response_img.content
+
+
+		soup = BeautifulSoup(page_img, "html.parser")
+
+		img_tag = soup.find("img")
+		partial_img_url = img_tag['src']
+		img_url = ('http://books.toscrape.com'+(partial_img_url.replace('../..',"")))
+		return img_url
+
+#titre du livre:
+reponse = requests.get(url)
+page = reponse.content
+
+soup = BeautifulSoup(page, "html.parser")
+
+title = soup.find("h1")
+book_title = title.text
+
+
+with open(book_title+'_pic.jpg', 'wb') as handle:
+        response = requests.get(get_img_url(url), stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+>>>>>>> 208db9a39569110214ce692e45090971c74f9285
             handle.write(block)
